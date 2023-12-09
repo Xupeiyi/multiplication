dc6cc89202e40ede18944ce76f89094379a56958
Peiyi Xu <xu_py311@outlook.com>
2023-12-09 17:28:41 -0500

    added repositories for User and ChallengeAttempt

--- src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
@@ -1,19 +1,47 @@
 package microservices.book.multiplication.challenge;
+import jakarta.persistence.*;
 import lombok.*;
 import microservices.book.multiplication.user.User;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 /**
  * Identifies the attempt from a {@link User} to solve a challenge.
  */
+
+@Entity
 @Getter
+@Setter
 @ToString
-@EqualsAndHashCode
+@RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeAttempt {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    @ToString.Exclude
     private User user;
     private int factorA;
     private int factorB;
     private int resultAttempt;
     private boolean correct;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ChallengeAttempt that = (ChallengeAttempt) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/microservices/book/multiplication/user/User.java
+++ src/main/java/microservices/book/multiplication/user/User.java
@@ -1,16 +1,46 @@
 package microservices.book.multiplication.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 /**
  * Stores information to identify the user.
  */
+
+@Entity
 @Getter
+@Setter
 @ToString
-@EqualsAndHashCode
+@RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String alias;
+
+    public User(final String userAlias) {
+        this(null, userAlias);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
