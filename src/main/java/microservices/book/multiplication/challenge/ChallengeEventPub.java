78855ee2f7a9a425a726bc2e325106f58353378a
Peiyi Xu <xu_py311@outlook.com>
2023-12-22 11:48:09 -0500

    removed direct call to gamification service

--- src/main/java/microservices/book/multiplication/challenge/ChallengeEventPub.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeEventPub.java
@@ -35,6 +35,4 @@ public class ChallengeEventPub {
                 attempt.getUser().getAlias()
         );
     }
-
-
 }
--- src/main/java/microservices/book/multiplication/challenge/ChallengeServiceImpl.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeServiceImpl.java
@@ -2,11 +2,11 @@ package microservices.book.multiplication.challenge;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import microservices.book.multiplication.serviceclients.GamificationServiceClient;
 import microservices.book.multiplication.user.User;
 import microservices.book.multiplication.user.UserRepository;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import java.util.List;
 
 
@@ -21,6 +21,7 @@ public class ChallengeServiceImpl implements ChallengeService {
 
     private final ChallengeEventPub challengeEventPub;
 
+    @Transactional
     @Override
     public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
         // check if the user already exists for that alias, otherwise create it
